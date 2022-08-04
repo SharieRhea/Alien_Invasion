3b85e039a3a71895b128c3030a4ae0cb97da5192
sharie.rhea <sharierhea@gmail.com>
2022-08-03 22:04:30 -0400

    Added scaling difficulty.

--- alien_invasion.py
+++ alien_invasion.py
@@ -68,6 +68,9 @@ class AlienInvasion:
         """Start a new game when the player clicks play."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            # Reset the game settings.
+            self.settings.initialize_dynamic_settings()
+            
             # Reset game statistics.
             self.stats.reset_stats()
             self.stats.game_active = True
@@ -128,6 +131,7 @@ class AlienInvasion:
             # Destroy the existing bullets and create a new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
